a1625479e37ccf8bdcaf66d1e3554655b0226d12
Tyguy047 <personal@tylercaselli.com>
2025-01-29 20:04:02 -0500

    Version 1.0.0 ov HacTix.tech Website

---
+++ media/scripts/inital-type.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const typedText = "Cyber-security & Ethical-Hacking tools in the palm of your hand...";
+    let index = 0;
+    const speed = 50; // typing speed in milliseconds
+
+    function typeWriter() {
+        if (index < typedText.length) {
+            document.getElementById("inital-type").innerHTML += typedText.charAt(index);
+            index++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+    typeWriter();
+});
